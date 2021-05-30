2ea942d2c4a816dfb198a406f4d06be64d670690
Nabanita28 <nab07.09@gmail.com>
2021-05-30 18:46:51 +0530

    Added swagger configuration

--- src/main/java/com/games/chess/controller/ChessController.java
+++ src/main/java/com/games/chess/controller/ChessController.java
@@ -1,16 +1,18 @@
 package com.games.chess.controller;
 
 import com.games.chess.game.GameDTO;
-import com.games.chess.game.Location;
-import com.games.chess.game.PieceType;
 import com.games.chess.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import javax.validation.Valid;
 
 @RestController
+@RequestMapping("chess/api/v1")
 public class GameController {
 
     private final GameService gameService;
@@ -20,14 +22,14 @@ public class GameController {
         this.gameService = gameService;
     }
 
-    @GetMapping("/start")
+    @GetMapping(path = "/start", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<GameDTO> startGame(){
         GameDTO gameDTO = gameService.startGame();
         return ResponseEntity.ok((gameDTO));
     }
 
-    @PostMapping("/move")
-    public ResponseEntity<GameDTO> nextMove(GameDTO gameDTO){
+    @PostMapping(path = "/move", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<GameDTO> nextMove(@Valid GameDTO gameDTO){
         GameDTO nextMoveGameDTO = gameService.nextMove(gameDTO);
         return ResponseEntity.ok(nextMoveGameDTO);
     }
