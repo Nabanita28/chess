1a176e7ea4fe289b7db821bdb55c88048b146220
Nabanita28 <nab07.09@gmail.com>
2021-05-31 01:46:52 +0530

    Corrected coordinates of the board

--- src/main/java/com/games/chess/service/GameService.java
+++ src/main/java/com/games/chess/service/GameService.java
@@ -31,13 +31,13 @@ public class GameService {
     public Piece[][] createNewBoard(Piece[][] board){
 
         board[0][0] = new Rook(Player.PLAYER2, new Location(0, 0), Colour.BLACK);
-        board[0][0] = new Knight(Player.PLAYER2, new Location(0, 1), Colour.BLACK);
-        board[0][0] = new Bishop(Player.PLAYER2, new Location(0, 2), Colour.BLACK);
-        board[0][0] = new Queen(Player.PLAYER2, new Location(0, 3), Colour.BLACK);
-        board[0][0] = new King(Player.PLAYER2, new Location(0, 4), Colour.BLACK);
-        board[0][0] = new Bishop(Player.PLAYER2, new Location(0, 5), Colour.BLACK);
-        board[0][0] = new Knight(Player.PLAYER2, new Location(0, 6), Colour.BLACK);
-        board[0][0] = new Rook(Player.PLAYER2, new Location(0, 7), Colour.BLACK);
+        board[0][1] = new Knight(Player.PLAYER2, new Location(0, 1), Colour.BLACK);
+        board[0][2] = new Bishop(Player.PLAYER2, new Location(0, 2), Colour.BLACK);
+        board[0][3] = new Queen(Player.PLAYER2, new Location(0, 3), Colour.BLACK);
+        board[0][4] = new King(Player.PLAYER2, new Location(0, 4), Colour.BLACK);
+        board[0][5] = new Bishop(Player.PLAYER2, new Location(0, 5), Colour.BLACK);
+        board[0][6] = new Knight(Player.PLAYER2, new Location(0, 6), Colour.BLACK);
+        board[0][7] = new Rook(Player.PLAYER2, new Location(0, 7), Colour.BLACK);
 
         for(int i=1; i<2; i++){
             for(int j=0; j<8; j++){
@@ -45,14 +45,14 @@ public class GameService {
             }
         }
 
-        board[0][0] = new Rook(Player.PLAYER1, new Location(7, 0), Colour.WHITE);
-        board[0][0] = new Knight(Player.PLAYER1, new Location(1, 1), Colour.WHITE);
-        board[0][0] = new Bishop(Player.PLAYER1, new Location(2, 2), Colour.WHITE);
-        board[0][0] = new Queen(Player.PLAYER1, new Location(3, 3), Colour.WHITE);
-        board[0][0] = new King(Player.PLAYER1, new Location(4, 4), Colour.WHITE);
-        board[0][0] = new Bishop(Player.PLAYER1, new Location(5, 5), Colour.WHITE);
-        board[0][0] = new Knight(Player.PLAYER1, new Location(6, 6), Colour.WHITE);
-        board[0][0] = new Rook(Player.PLAYER1, new Location(7, 7), Colour.WHITE);
+        board[7][0] = new Rook(Player.PLAYER1, new Location(7, 0), Colour.WHITE);
+        board[7][1] = new Knight(Player.PLAYER1, new Location(7, 1), Colour.WHITE);
+        board[7][2] = new Bishop(Player.PLAYER1, new Location(7, 2), Colour.WHITE);
+        board[7][3] = new Queen(Player.PLAYER1, new Location(7, 3), Colour.WHITE);
+        board[7][4] = new King(Player.PLAYER1, new Location(7, 4), Colour.WHITE);
+        board[7][5] = new Bishop(Player.PLAYER1, new Location(7, 5), Colour.WHITE);
+        board[7][6] = new Knight(Player.PLAYER1, new Location(7, 6), Colour.WHITE);
+        board[7][7] = new Rook(Player.PLAYER1, new Location(7, 7), Colour.WHITE);
 
         for(int i=6; i<7; i++){
             for(int j=0; j<8; j++){
